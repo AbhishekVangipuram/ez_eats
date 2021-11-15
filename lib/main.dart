e42feb4eb142dd1c7ef3870aa040be6bae7b21fe
Abhishek Vangipuram <abhivangipuram@gmail.com>
2021-11-15 13:56:51 -0500

    added more firebase necessities

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,129 @@
-import 'package:english_words/english_words.dart';
+// import 'package:english_words/english_words.dart';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Startup Name Generator',
-        theme: ThemeData(
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Colors.white,
-            foregroundColor: Colors.black,
-          ),
-        ),
-        home: RandomWords());
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Define an async function to initialize FlutterFire
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize
+      await Firebase.initializeApp();
+    } catch (e) {
+      log(e.toString());
+    }
   }
-}
-
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
 
   @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
 
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFontStyle = const TextStyle(fontSize: 18.0);
-  final _saved = <WordPair>{};
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Startup Name Generator'), actions: [
-          IconButton(
-            onPressed: _pushSaved,
-            icon: const Icon(Icons.list),
-            tooltip: "Saved Suggestions",
-          )
-        ]),
-        body: _buildSuggestions());
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return const Divider();
-          final index = i ~/ 2; // i // 2
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair wordPair) {
-    final alreadySaved = _saved.contains(wordPair);
-    return ListTile(
-        title: Text(wordPair.asPascalCase, style: _biggerFontStyle),
-        trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
-            color: alreadySaved ? Colors.red : null,
-            semanticLabel: alreadySaved ? "Remove from saved" : "save"),
-        onTap: () {
-          setState(() {
-            if (alreadySaved) {
-              _saved.remove(wordPair);
-            } else {
-              _saved.add(wordPair);
-            }
-          });
-        });
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-      final tiles = _saved.map((pair) =>
-          ListTile(title: Text(pair.asPascalCase, style: _biggerFontStyle)));
-      final divided = tiles.isNotEmpty
-          ? ListTile.divideTiles(tiles: tiles, context: context).toList()
-          : <Widget>[];
-      return Scaffold(
-          appBar: AppBar(
-            title: const Text("Saved Suggestions"),
-          ),
-          body: ListView(children: divided));
-    }));
+        appBar: AppBar(
+      title: const Text("Firebase Demo"),
+    ));
   }
 }
+
+
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         title: 'Startup Name Generator',
+//         theme: ThemeData(
+//           appBarTheme: const AppBarTheme(
+//             backgroundColor: Colors.white,
+//             foregroundColor: Colors.black,
+//           ),
+//         ),
+//         home: RandomWords());
+//   }
+// }
+
+// class RandomWords extends StatefulWidget {
+//   const RandomWords({Key? key}) : super(key: key);
+
+//   @override
+//   _RandomWordsState createState() => _RandomWordsState();
+// }
+
+// class _RandomWordsState extends State<RandomWords> {
+//   final _suggestions = <WordPair>[];
+//   final _biggerFontStyle = const TextStyle(fontSize: 18.0);
+//   final _saved = <WordPair>{};
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(title: const Text('Startup Name Generator'), actions: [
+//           IconButton(
+//             onPressed: _pushSaved,
+//             icon: const Icon(Icons.list),
+//             tooltip: "Saved Suggestions",
+//           )
+//         ]),
+//         body: _buildSuggestions());
+//   }
+
+//   Widget _buildSuggestions() {
+//     return ListView.builder(
+//         padding: const EdgeInsets.all(16.0),
+//         itemBuilder: (context, i) {
+//           if (i.isOdd) return const Divider();
+//           final index = i ~/ 2; // i // 2
+//           if (index >= _suggestions.length) {
+//             _suggestions.addAll(generateWordPairs().take(10));
+//           }
+//           return _buildRow(_suggestions[index]);
+//         });
+//   }
+
+//   Widget _buildRow(WordPair wordPair) {
+//     final alreadySaved = _saved.contains(wordPair);
+//     return ListTile(
+//         title: Text(wordPair.asPascalCase, style: _biggerFontStyle),
+//         trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+//             color: alreadySaved ? Colors.red : null,
+//             semanticLabel: alreadySaved ? "Remove from saved" : "save"),
+//         onTap: () {
+//           setState(() {
+//             if (alreadySaved) {
+//               _saved.remove(wordPair);
+//             } else {
+//               _saved.add(wordPair);
+//             }
+//           });
+//         });
+//   }
+
+//   void _pushSaved() {
+//     Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+//       final tiles = _saved.map((pair) =>
+//           ListTile(title: Text(pair.asPascalCase, style: _biggerFontStyle)));
+//       final divided = tiles.isNotEmpty
+//           ? ListTile.divideTiles(tiles: tiles, context: context).toList()
+//           : <Widget>[];
+//       return Scaffold(
+//           appBar: AppBar(
+//             title: const Text("Saved Suggestions"),
+//           ),
+//           body: ListView(children: divided));
+//     }));
+//   }
+// }
