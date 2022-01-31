6692b3dc17711ebf70ca7c3b0bf552268eb20ecd
Abhishek Vangipuram <abhivangipuram@gmail.com>
2022-01-31 12:02:18 -0500

    misc

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:ez_eats/screens/add_user_screen.dart';
 import 'package:ez_eats/screens/user_list_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 // import 'package:firebase_core/firebase_core.dart';
 // import 'package:cloud_firestore/cloud_firestore.dart';
 
